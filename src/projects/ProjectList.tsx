e6531f9f108384e575b45a590106bce370652979
FreeWind94 <shukin-nikita@mail.ru>
2023-08-18 23:03:07 +0300

    Communicating from Child to Parent Component

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -7,11 +7,17 @@ interface ProjectsListProps {
 }
 
 function ProjectList({ projects }: ProjectsListProps) {
+    const handleEdit = (project: Project) => {
+        console.log(project);
+    }
+
     return (
         <div className="row">
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
-                    <ProjectCard project={project}></ProjectCard>
+                    <ProjectCard
+                        project={project}
+                        onEdit={handleEdit}></ProjectCard>
                     <ProjectForm />
                 </div>
             ))}
