5148dc80cb98785fd262962a4218737af369bebe
FreeWind94 <shukin-nikita@mail.ru>
2023-09-02 01:10:19 +0300

    Custom Hooks

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,67 +1,32 @@
-import { Fragment, useEffect, useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
-import { Project } from "./Project";
 import ProjectList from "./ProjectList";
-import { projectAPI } from "./projectAPI";
+import { useProjects } from "./projectHooks";
 
 function ProjectsPage() {
-    const [projects, setProjects] = useState<Project[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | undefined>(undefined);
-    const [currentPage, setCurrentPage] = useState(1);
-
-    useEffect(() => {
-        async function loadPojects() {
-            setLoading(true);
-            try {
-                const data = await projectAPI.get(currentPage);
-                setError('');
-                if (currentPage === 1) {
-                    setProjects(data);
-                } else {
-                    setProjects((projects) => [...projects, ...data]);
-                }
-            } catch (e) {
-                if (e instanceof Error) {
-                    setError(e.message);
-                }
-            } finally {
-                setLoading(false);
-            }
-        }
-        loadPojects();
-    }, [currentPage]);
-
-    const saveProject = (project: Project) => {
-        projectAPI
-            .put(project)
-            .then((updatedProject) => {
-                let updatedProjects = projects.map((p: Project) => {
-                    return p.id === project.id ? project : p;
-                });
-                setProjects(updatedProjects);
-            })
-            .catch((e) => {
-                if (e instanceof Error) {
-                    setError(e.message);
-                }
-            });
-    };
+    const {
+        projects,
+        loading,
+        error,
+        setCurrentPage,
+        saveProject,
+        saving,
+        savingError,
+    } = useProjects();
 
     const handleMoreClick = () => {
         setCurrentPage((currentPage) => currentPage + 1);
     };
 
     return (
-        <Fragment>
+        <>
             <h1>Projects</h1>
-            {error && (
+            {saving && <span className="toast">Saving...</span>}
+            {(error || savingError) && (
                 <div className="row">
                     <div className="card large error">
                         <section>
                             <p>
                                 <span className="icon-alert inverce"></span>
-                                {error}
+                                {error} {savingError}
                             </p>
                         </section>
                     </div>
@@ -85,7 +50,7 @@ function ProjectsPage() {
                     <p>Loading...</p>
                 </div>
             )}
-        </Fragment>
+        </>
     );
 }
 
