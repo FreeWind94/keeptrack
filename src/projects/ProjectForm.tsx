97d3fc951b6a722581e9cfc9060f1611e4c02b69
FreeWind94 <shukin-nikita@mail.ru>
2023-09-04 23:28:25 +0300

    React Query Refactor

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,15 +1,14 @@
 import { SyntheticEvent, useState } from "react";
 import { Project } from "./Project";
+import { useSaveProject } from "./projectHooks";
 
 interface ProjectFormProps {
     project: Project;
-    onSave: (project: Project) => void;
     onCancel: () => void;
 }
 
 function ProjectForm({
     project: initialProject,
-    onSave,
     onCancel,
 }: ProjectFormProps) {
     const [project, setProject] = useState(initialProject);
@@ -19,12 +18,13 @@ function ProjectForm({
         budget: '',
     });
 
+    const { mutate: saveProject, isLoading } = useSaveProject();
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
         if (!isValid()) {
             return;
         }
-        onSave(project);
+        saveProject(project);
     };
 
     const handleChange = (event: any) => {
@@ -81,6 +81,7 @@ function ProjectForm({
 
     return (
         <form className="input-group vertical" onSubmit={handleSubmit}>
+            {isLoading && <span className="toast">Saving...</span>}
             <label htmlFor="name">Project Name</label>
             <input
                 type="text"
